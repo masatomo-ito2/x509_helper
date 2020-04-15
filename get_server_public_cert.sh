67463a2d3ecf27b4ee72cd677afc0d6e395fe0cd
masa3 <masa@hashicorp.com>
2020-04-15 11:52:35 +0900

    few modificatin

--- get_server_public_cert.sh
+++ get_server_public_cert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#if [ $# -nq 0 ]
-#	then
-#		echo 'Needs to supply argument'
-#		echo '  $1 = <arg>'
-#		exit 1
-#fi
+if [ $# -nq 1 ]
+	then
+		echo 'Needs to supply argument'
+		echo '  $1 = server'
+		exit 1
+fi
 
-openssl s_client -showcerts -connect masatomo.ito:443 </dev/null 2>/dev/null |openssl x509 -outform PEM >mycertfile.pem 
+openssl s_client -showcerts -connect ${1}:443 </dev/null 2>/dev/null |openssl x509 -outform PEM >mycertfile.pem 
