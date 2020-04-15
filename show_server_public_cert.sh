8743ce83188f9fee8dc1c995dd3e3c6e5ccf731a
masa3 <masa@hashicorp.com>
2020-04-15 18:50:20 +0900

    add argument

--- show_server_public_cert.sh
+++ show_server_public_cert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#if [ $# -nq 0 ]
-#	then
-#		echo 'Needs to supply argument'
-#		echo '  $1 = <arg>'
-#		exit 1
-#fi
+if [ $# -ne 1 ]
+	then
+		echo 'Needs to supply argument'
+		echo '  $1 = server'
+		exit 1
+fi
 
-openssl s_client -connect masatomo.ito:443 -showcerts 
+openssl s_client -connect ${1}:443 -showcerts 
